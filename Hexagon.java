3451dc47c68140c25fbaba5a21c7a0b4da458627
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2023-04-11 17:26:14 -0400

    Update Hexagon.java

--- Hexagon.java
+++ Hexagon.java
@@ -1,23 +1,21 @@
 /*
-* Description: Prompts the user to enter the side of a hexagon and displays its area
-*/
+ * Description: Prompts the user to enter the side of a hexagon and displays its area
+ */
 
 import java.util.Scanner;
 
 public class Hexagon {
-
-   public static void main (String[] args) {
+   public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
-      
+
       // Prompt the user to enter the side of a hexagon
-      System.out.print("Enter the side: ");
+      System.out.print("Enter the side of a hexagon: ");
       double side = input.nextDouble();
-      
+
       // Compute the area of the hexagon
-      double area = ((3 * Math.pow(3, 0.5)) / 2) * Math.pow(side, 2);
-      
-      // Display result
-      System.out.printIn("The area of a hexagon is " +
-      area);
-      }
+      double area = (3 * Math.sqrt(3) * Math.pow(side, 2)) / 2;
+
+      // Display the result
+      System.out.println("The area of the hexagon is " + area);
    }
+}
